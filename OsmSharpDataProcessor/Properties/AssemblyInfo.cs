7fe91027de7fa6a55a57595c3a3d3747c73e0a7e
xivk <ben@osmsharp.com>
2014-04-09 18:23:10 +0200

    Updated to latest OsmSharp release. Added releasenotes.

--- OsmSharpDataProcessor/Properties/AssemblyInfo.cs
+++ OsmSharpDataProcessor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("SharpSoftware")]
 [assembly: AssemblyProduct("OsmSharpDataProcessor")]
-[assembly: AssemblyCopyright("Copyright © Ben Abelshausen")]
+[assembly: AssemblyCopyright("Copyright © OsmSharp")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d8cb0955-89c7-4bc4-9e62-a369e343363c")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
